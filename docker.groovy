15b83f884c4232a851d1757b5eac8eecbeb10e34
Andrew Balandin <formulaflower@ya.ru>
2020-02-02 20:16:14 +0300

    add pipeline with docker

---
+++ docker.groovy
@@ -0,0 +1,25 @@
+#!groovy
+properties([disableConcurrentBuilds()])
+
+pipeline {
+    agent {
+        label 'master'
+        }
+    options {
+        buildDiscarder(logRotator(numToKeepStr: '10', artifactNumToKeepStr: '10'))
+        timestamps()
+    }
+    stages {
+        stage("First step") {
+            steps {
+                sh 'scp /home/ubuntu/docker/Dockerfile root@ubuntu2:/home/ubuntu/docker/alpine/'
+                sh 'ssh root@ubuntu2 \'docker build /home/ubuntu/docker/alpine/\''
+            }
+        }
+        stage("Second step") {
+            steps {
+                sh 'ssh root@ubuntu2 \'uptime\''
+            }
+        }
+    }
+}
